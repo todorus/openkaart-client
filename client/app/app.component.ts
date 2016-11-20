5db45ce159d82a59366c30e4acc8c1e24d7b8d6c
Theo van den Bogaart <todorus@todorus.com>
2016-11-20 20:13:47 +0100

    styled sidemenu

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'app',
-  templateUrl: 'app/app.html'
+  templateUrl: 'app/app.html',
+  styleUrls: ['app/app.css']
 })
 export class AppComponent { }
